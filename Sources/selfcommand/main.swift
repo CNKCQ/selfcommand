98c53d87c45f53ea5ece18c5e70083aa16fa93b2
steve <chengquan.wang@ele.me>
2018-07-17 21:01:06 +0800

    color for commandline

--- Sources/selfcommand/main.swift
+++ Sources/selfcommand/main.swift
@@ -1,4 +1,47 @@
+import Foundation
 import CommandLineKit
 import Rainbow
 
-print("fuck you   Hello, world!")
+
+
+let cli = CommandLineKit.CommandLine()
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    return cli.defaultFormat(s: str, type: type)
+}
+
+
+
+let filePath = StringOption(shortFlag: "f", longFlag: "file", required: true,
+                            helpMessage: "Path to the output file.")
+let compress = BoolOption(shortFlag: "c", longFlag: "compress",
+                          helpMessage: "Use data compression.")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+let verbosity = CounterOption(shortFlag: "v", longFlag: "verbose",
+                              helpMessage: "Print verbose messages. Specify multiple times to increase verbosity.")
+
+cli.addOptions(filePath, compress, help, verbosity)
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+print("File path is \(filePath.value!)")
+print("Compress is \(compress.value)")
+print("Verbosity is \(verbosity.value)")
+
