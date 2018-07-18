10b715f6094f5cf0a1875f226d4d41c61c36580b
steve <steve@stevedeMBP.lan>
2018-07-18 23:20:15 +0800

    update: test arg

--- Package.swift
+++ Package.swift
@@ -7,14 +7,13 @@ let package = Package(
     name: "selfcommand",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.1"),
-        .package(url: "https://github.com/jatoben/CommandLine.git", from: "3.0.0-pre1")
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "selfcommand",
-            dependencies: ["Rainbow", "CommandLine"]),
+            dependencies: ["Rainbow"]),
     ]
 )
--- Sources/selfcommand/main.swift
+++ Sources/selfcommand/main.swift
@@ -1,47 +1,17 @@
 import Foundation
-import CommandLineKit
 import Rainbow
 
 
 
-let cli = CommandLineKit.CommandLine()
+let inputs = CommandLine.arguments
 
-cli.formatOutput = { s, type in
-    var str: String
-    switch(type) {
-    case .error:
-        str = s.red.bold
-    case .optionFlag:
-        str = s.green.underline
-    case .optionHelp:
-        str = s.blue
-    default:
-        str = s
+if inputs.count < 2 {
+    print("welcome to the space".underline.lightGreen)
+}
+
+for index in 0..<inputs.count {
+    if index < 1 {
+        continue
     }
-    return cli.defaultFormat(s: str, type: type)
+    print("\n\(inputs[index])".underline.lightGreen)
 }
-
-
-
-let filePath = StringOption(shortFlag: "f", longFlag: "file", required: true,
-                            helpMessage: "Path to the output file.")
-let compress = BoolOption(shortFlag: "c", longFlag: "compress",
-                          helpMessage: "Use data compression.")
-let help = BoolOption(shortFlag: "h", longFlag: "help",
-                      helpMessage: "Prints a help message.")
-let verbosity = CounterOption(shortFlag: "v", longFlag: "verbose",
-                              helpMessage: "Print verbose messages. Specify multiple times to increase verbosity.")
-
-cli.addOptions(filePath, compress, help, verbosity)
-
-do {
-    try cli.parse()
-} catch {
-    cli.printUsage(error)
-    exit(EX_USAGE)
-}
-
-print("File path is \(filePath.value!)")
-print("Compress is \(compress.value)")
-print("Verbosity is \(verbosity.value)")
-
